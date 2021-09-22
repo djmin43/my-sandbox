88ac39d8cab7b5457d9c956c47f011d1309d22e6
james <james.min@travelFlan.com>
2021-09-23 08:09:54 +0900

    callback function

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-var value1 = 3
-var value2 = value1
-
-function value3 () {
-  return value1 - 2
+const callBackFunction = (value, func) => {
+  console.log(value)
+  func(value)
 }
 
-var value4 = value3()
-
-console.log(value1, value2, value4)
+const echo = (number) => {
+  console.log('callback', number)
+}
+callBackFunction(1, echo)
