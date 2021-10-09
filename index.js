ae52a64c29d268176fddd258d87e91ab39f73541
jay <dj.min43@gmail.com>
2021-10-09 16:42:54 +0900

    combine fns

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
-const dan = {
-  type: "person",
-  data: {
-    gender: "male",
-    info: {
-      id: 22,
-      fullname: {
-        first: "Dan",
-        last: "Deacon"
-      }
-    }
-  }
+const comps = (...fns) => {
+  fns.forEach(fn => fn())
 }
 
-const deepPick = (fields, object = {}) => {
-  const [first, ...remaining] = fields.split('.');
-  return (remaining.length) ?
-    deepPick(remaining.join('.'), object[first])
-    : object[first]
-}
+const hello = () => console.log('hello')
+const bye = () => console.log('bye')
 
-console.log(deepPick("type", dan))
+comps(hello, bye)
