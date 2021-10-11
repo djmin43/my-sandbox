f7accebf571f16397dbdd6d2d88fd533db8eb0ab
jay <dj.min43@gmail.com>
2021-10-11 20:57:21 +0900

    testing out useeffect

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import colorData from '../static/json/color-data.json'
 import ColorList from './components/ColorList'
@@ -6,18 +6,28 @@ import StarRating from './components/StarRating'
 
 function App() {
   const [colors, setColors] = useState(colorData)
+  const [name, setName] = useState('jay')
 
   const onRemoveColor = (id: string) => {
     const newColors = colors.filter(color => color.id !== id)
     setColors(newColors)
   }
+
+  useEffect(() =>{
+    setName('hello')
+    console.log(name)}
+  , [name])
   
   return (
     <div className="App">
+      <button onClick={() => setName('something else')}>
+        change
+      </button>
       <ColorList
         onRemoveColor={onRemoveColor}
         colors={colors}
       />
+
     </div>
   )
 }
