0707b731ff980c51a41a7f869a1fa8ea423add14
jay <dj.min43@gmail.com>
2021-10-14 09:16:56 +0900

    minor css decoration

--- redux-todo/src/App.tsx
+++ redux-todo/src/App.tsx
@@ -1,28 +1,47 @@
-import React, { useState, useEffect } from 'react'
+import React, { ReactElement, useState } from 'react'
 import { add, remove, toggle } from './reducers/todoSlice'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from './reducers'
 import { v4 as uuidv4 } from 'uuid'
+import styled from '@emotion/styled'
+
+const Container = styled.div`
+  padding: 50px;
+  text-align: center;
+`
+
+const TodoItemText = styled.span<{isFinished: boolean}>`
+  font-size: calc(5px + 5vw);
+  color: ${({isFinished}) => isFinished ? 'green' : 'red'};
+  animation: pop 1s ease-in-out;
+
+  @keyframes pop {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+`
 
 function App() {
 
   const dispatch = useDispatch()
   const [todoItem, setTodoItem] = useState('')
 
-  const result = useSelector((state: RootState) =>
-    state.todos.todo
-  )
-
+  const result = useSelector((state: RootState) => state.todos.todo)
 
   const handleSubmit = (e: React.SyntheticEvent): void => {
     e.preventDefault()
+    console.log(uuidv4())
     const newTodoItem = {
       id: uuidv4(),
       todo: todoItem,
       isFinished: false,
     }
-    console.log(uuidv4())
     dispatch(add(newTodoItem))
+    setTodoItem('')
   }
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -38,23 +57,22 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <Container>
       <form onSubmit={handleSubmit}>
-        <label>add todo</label>
+        <label>할 일을 적어주세요:</label>
         <input type="text" value={todoItem} onChange={handleChange}/>
-        <button>add item</button>
+        <button>추가</button>
       </form>
       <div>
-        {result.map(item =>
-        <div key={item.id}>
-          <span>{item.todo}</span>
-          <span>{item.isFinished ? 'true' : 'false'}</span>
+        {result.map((item, index) =>
+        <div key={item.id} onClick={() => toggleTodoItem(item.id)}>
+          <span>{index + 1}.</span>
+          <TodoItemText isFinished={item.isFinished}>{item.todo}</TodoItemText>
           <button onClick={() => removeTodoItem(item.id)}>remove</button>
-          <button onClick={() => toggleTodoItem(item.id)}>toggle</button>
         </div>
           )}
       </div>
-    </div>
+    </Container>
   )
 }
 
