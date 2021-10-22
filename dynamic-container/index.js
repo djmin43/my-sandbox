f9ca41853c5acaf065514322e98c1c8fe36e119a
jay <dj.min43@gmail.com>
2021-10-22 17:01:43 +0900

    write up a docker compose yaml

--- dynamic-container/index.js
+++ dynamic-container/index.js
@@ -1,6 +1,6 @@
 const { exec } = require("child_process")
 
-exec('sh ./my-script.sh', (error, stdout, stderr) => {
+exec('echo hello', (error, stdout, stderr) => {
   var id = 1234
   if (error) {
     console.log(`exec error: ${error}`)
