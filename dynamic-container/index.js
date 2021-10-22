92491fc982d5e1c73dd4b5b383025419e3403471
jay <dj.min43@gmail.com>
2021-10-22 16:35:25 +0900

    index variable

--- dynamic-container/index.js
+++ dynamic-container/index.js
@@ -1,6 +1,7 @@
 const { exec } = require("child_process")
 
 exec('sh ./my-script.sh', (error, stdout, stderr) => {
+  var id = 1234
   if (error) {
     console.log(`exec error: ${error}`)
     return
