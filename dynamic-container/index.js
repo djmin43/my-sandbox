a6b072692262d03bbc43571eadb01ae23bc9fc3f
jay <dj.min43@gmail.com>
2021-10-22 16:10:16 +0900

    scripting works

--- dynamic-container/index.js
+++ dynamic-container/index.js
@@ -1,3 +1,10 @@
 const { exec } = require("child_process")
 
-exec()
+exec('sh ./my-script.sh', (error, stdout, stderr) => {
+  if (error) {
+    console.log(`exec error: ${error}`)
+    return
+  }
+  console.log(`stdout: ${stdout}`)
+  console.error(`stderr: ${stderr}`)
+})
