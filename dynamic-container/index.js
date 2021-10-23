1747cdcfa526c113b89bd86e8e6cde1d9e07318a
jay <dj.min43@gmail.com>
2021-10-23 22:34:21 +0900

    express set up for listen 5000

--- dynamic-container/index.js
+++ dynamic-container/index.js
@@ -1,25 +1,37 @@
 const { exec } = require("child_process")
 const createDockerFile = require('./file-creator')
 const fs = require("fs")
+const express = require("express")
+const app = express()
+const port = 5000
 
-var id = 1234
-
-fs.writeFile('./docker-folder/Dockerfile', createDockerFile(id), function (err) {
-  if (err) {
-    console.log(err)
-    return
-  }
-  console.log('yay!')
-  runDocker(id)
+app.get('/', (req, res) => {
+  res.send('hello world!')
 })
 
+app.listen(port, () => {
+  console.log(`you are on port ${port}`)
+})
 
-const runDocker = (id) => {
-  exec(`docker build -t testing-${id} ./docker-folder`, (error, stdout, stderr) => {
-  if (error) {
-    console.log(`exec error: ${error}`)
-    return
-  }
-  console.log(`stdout: ${stdout}`)
-  console.error(`stderr: ${stderr}`)
-})}
+console.log('hello')
+// var id = 1234
+
+// fs.writeFile('./docker-folder/Dockerfile', createDockerFile(id), function (err) {
+//   if (err) {
+//     console.log(err)
+//     return
+//   }
+//   console.log('yay!')
+//   runDocker(id)
+// })
+
+
+// const runDocker = (id) => {
+//   exec(`docker build -t testing-${id} ./docker-folder`, (error, stdout, stderr) => {
+//   if (error) {
+//     console.log(`exec error: ${error}`)
+//     return
+//   }
+//   console.log(`stdout: ${stdout}`)
+//   console.error(`stderr: ${stderr}`)
+// })}
