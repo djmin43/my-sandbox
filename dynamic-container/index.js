2bace8f155a1543d138c1e4efa8674bea242495b
jay <dj.min43@gmail.com>
2021-10-23 22:44:17 +0900

    super basic post and get request from express

--- dynamic-container/index.js
+++ dynamic-container/index.js
@@ -5,8 +5,16 @@ const express = require("express")
 const app = express()
 const port = 5000
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
+  console.log('req', req)
   res.send('hello world!')
+})
+
+app.post('/', (req, res) => {
+  console.log('req', req.body.hello)
+  res.send('hello')
 })
 
 app.listen(port, () => {
