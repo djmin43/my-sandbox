54cc9ee4df15d0e065b03d65ffefc2df380cce41
jay <dj.min43@gmail.com>
2021-10-24 11:40:42 +0900

    issues with port number

--- dynamic-container/dockerfile-creator.js
+++ dynamic-container/dockerfile-creator.js
@@ -1,13 +1,18 @@
 const createDockerFile = (payload) => {
   console.log('createdockerfile', payload)
   const file = `
-  FROM node:alpine AS deps
-  WORKDIR /app
+  FROM node:alpine
+  RUN mkdir -p /usr/src
+  WORKDIR /usr/src
+  COPY . /usr/src  
   ENV SN=${payload.sn}
   ENV MESSAGE="${payload.message}"
+  COPY package.json yarn.lock ./
+  RUN yarn install
+  RUN yarn build
   COPY . .
   EXPOSE ${payload.port}
-  CMD ["yarn", "dev"]
+  CMD ["yarn", "start"]
   `
   return file
 }
