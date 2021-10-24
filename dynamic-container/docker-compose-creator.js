e8734d110df4a77471c45e9012ffb82535ea20c3
jay <dj.min43@gmail.com>
2021-10-24 11:18:47 +0900

    add js-yaml

--- dynamic-container/docker-compose-creator.js
+++ dynamic-container/docker-compose-creator.js
@@ -1,10 +1,10 @@
 const createDockerCompose = (payload) => {
   const dockerCompose = `
-  version: "3.9"
-    front-end:
-      build: ./template-${payload.template}
-      ports:
-        - "${payload.port}:${payload.port}"
+version: "3.9"
+  front-end:
+    build: ./template-${payload.template}
+    ports:
+      - "${payload.port}:${payload.port}"
   `
   return dockerCompose
 }
