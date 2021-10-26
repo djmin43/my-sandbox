adac9b80b814912e679553cffa0c621d6cb810f4
jay <dj.min43@gmail.com>
2021-10-27 08:03:15 +0900

    notes to each component of go

--- go-lang-try/main.go
+++ go-lang-try/main.go
@@ -1,7 +1,12 @@
 package main
 
+// package form go standard library. fmt -> formatting strings and prnting messages to console.
+// method after import package should start with Capital.
 import "fmt"
 
+// go will look for main at first. there should be only one main function in the entry file.
 func main() {
+
 	fmt.Println("hello world!")
+
 }
