369194e218b8123cf68f95113830c3ca2ccaffba
jay <dj.min43@gmail.com>
2021-10-30 13:19:06 +0900

    install nodemon

--- wrong-abstraction/index.js
+++ wrong-abstraction/index.js
@@ -3,21 +3,43 @@
 // Kent C Dodds - https://kentcdodds.com/blog/aha-programming
 // Dan Abramov  - https://overreacted.io/the-wet-codebase/
 
-const userName = {
-  first: 'Jay',
-  last: 'Min',
+const user = {
+  firstName: 'Jay',
+  lastName: 'Min',
   favouriteBand: 'Black Sabbath',
   favouriteMovie: 'Baby Driver',
   age: 33
 }
 
-// CASE #1: no abstraction
+const userOne = {
+  firstName: 'Michael',
+  lastName: 'Jordan',
+  favouriteBand: 'Himself',
+  favouriteMovie: 'Gladiator',
+  age: 92
+}
 
+const userTwo = {
+  firstName: 'Michael',
+  lastName: 'Jackson',
+  favouriteBand: null,
+  favouriteMovie: null,
+  age: 59
+}
+
+// CASE #1: no abstraction
 const displayNameOnly = 'Jay Min'
 const displayNameWithAge = 'Jay Min, 33'
 const displayNameAndFavouriteMovie = 'Jay Min, favourite movie: Baby Driver'
 // What happens if user name changes?
 
-// CAASE #2: hard coding is bad. 
+// CASE #2: hard coding is bad. it's time to program!
+const userFullName = `${user.firstName} ${user.lastName}`
+const displayNameWithAge = `${userFullName} ${user.age}`
+const displayNameAndFavouriteMovie = `${userFullName}, favourite movie: ${user.favouriteMovie}`
 
-const displayNameOnly = 
+// CASE #3: what if there is more than one user? abstraction comes to rescue!
+const getFullUserName = (user) => `${user.firstName} ${user.lastName}`
+const getFullInformation = (user) => `${getFullUserName(user)} `
+const userOneFullName = getFullUserName(userOne)
+const userTwoFullName = getFullUserName(userTwo)
