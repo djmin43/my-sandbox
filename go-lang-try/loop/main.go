61318553800508e3987deee0d66841f9929986c0
jay <dj.min43@gmail.com>
2021-11-01 08:18:33 +0900

    go: add an empty arg

--- go-lang-try/loop/main.go
+++ go-lang-try/loop/main.go
@@ -21,8 +21,14 @@ func main() {
 	// 	fmt.Println(names[i])
 	// }
 
-	for index, value := range names {
-		fmt.Printf("the value at index %v is %v \n", index, value)
+	// for index, value := range names {
+	// 	fmt.Printf("the value at index %v is %v \n", index, value)
+	// }
+
+	for _, value := range names {
+		fmt.Printf("the value is %v \n", value)
 	}
+
+	
 }
 
