62ec0183a2efa6bda449fd0beffbf9000c7a2554
jay <dj.min43@gmail.com>
2021-11-01 06:55:29 +0900

    go: append to slices

--- go-lang-try/array-and-slices/main.go
+++ go-lang-try/array-and-slices/main.go
@@ -7,11 +7,14 @@ func main() {
 	var ages = [3]int{20, 25, 30}
 
 	names := [4]string{"yoshi", "mario", "peach", "bowser"}
+	names[1] = "luigi"
 
 	fmt.Println(ages, len(ages))
 	fmt.Println(names, len(names))
 
 	// slices (use arrays under the hood)
 	var scores = []int{100, 50, 60}
+	scores[2] = 25
+	scores = append(scores, 85)
 
 }
