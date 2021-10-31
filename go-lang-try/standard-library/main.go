84467bbc438c0806c0fa04706cfa1731d3cb4b76
jay <dj.min43@gmail.com>
2021-11-01 07:13:14 +0900

    go: to uppser

--- go-lang-try/standard-library/main.go
+++ go-lang-try/standard-library/main.go
@@ -8,5 +8,9 @@ import (
 func main() {
 	greeting := "hello there friends!"
 
-	fmt.Println(strings.Contains(greeting, "hello"))
+	// fmt.Println(strings.Contains(greeting, "hello"))
+	// fmt.Println(strings.ReplaceAll(greeting, "hello", "hi"))
+	fmt.Println(strings.ToUpper(greeting))
+
+
 }
