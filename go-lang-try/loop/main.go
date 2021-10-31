a8756d823043e8a2fef1cd2af145e1f985853319
jay <dj.min43@gmail.com>
2021-11-01 08:17:05 +0900

    go: loop map with index and value

--- go-lang-try/loop/main.go
+++ go-lang-try/loop/main.go
@@ -4,9 +4,25 @@ import "fmt"
 
 
 func main() {
-	x := 0
-	for x < 5 {
-		fmt.Println("value of x is:", x)
-		x++
+	// x := 0
+	// for x < 5 {
+	// 	fmt.Println("value of x is:", x)
+	// 	x++
+	// }
+
+
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println("value of x is:", i)
+	// }
+
+	names  := []string{"mario", "luigi", "yoshi", "peach"}
+
+	// for i := 0; i < len(names); i ++ {
+	// 	fmt.Println(names[i])
+	// }
+
+	for index, value := range names {
+		fmt.Printf("the value at index %v is %v \n", index, value)
 	}
 }
+
