c1485dd25e74a7c74cbff2ab5fc840dee38ef2f0
jay <dj.min43@gmail.com>
2021-11-01 06:51:52 +0900

    go: different way of calling an array

--- go-lang-try/array-and-slices/main.go
+++ go-lang-try/array-and-slices/main.go
@@ -6,6 +6,9 @@ func main() {
 	// var ages [3]int = [3]int{20, 25, 30}
 	var ages = [3]int{20, 25, 30}
 
+	names := [4]string{"yoshi", "mario", "peach", "bowser"}
+
 	fmt.Println(ages, len(ages))
+	fmt.Println(names, len(names))
 
 }
