86570e0d70b9d5a588c9921bc6ad189b04a7cfe1
jay <dj.min43@gmail.com>
2021-11-01 06:50:36 +0900

    go: array length

--- go-lang-try/array-and-slices/main.go
+++ go-lang-try/array-and-slices/main.go
@@ -1,6 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
-	var ages [3]int = [3]int{20, 25, 30}
+	// var ages [3]int = [3]int{20, 25, 30}
+	var ages = [3]int{20, 25, 30}
+
+	fmt.Println(ages, len(ages))
 
 }
