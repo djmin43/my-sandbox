e397859a564fdaa2dbd5aeb609b6a4c611238228
jay <dj.min43@gmail.com>
2021-11-01 08:41:07 +0900

    go: boolean done

--- go-lang-try/boolean/main.go
+++ go-lang-try/boolean/main.go
@@ -17,4 +17,20 @@ func main() {
 	} else {
 		fmt.Println("age is not less than 45")
 	}
+
+	names := []string{"mario", "luigi", "yoshi", "peach", "bowser"}
+
+	for index, value := range names {
+		if index == 1 {
+			fmt.Println("continuing at pos", index)
+			continue
+		}
+		if index >2 {
+			fmt.Println("breaking at pos", index)
+			break
+		}
+
+		fmt.Printf("the value at pos %v is %v \n", index, value)
+	}
+
 }
