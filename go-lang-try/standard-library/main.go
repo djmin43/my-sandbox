d8f25714f8522b91e1fc56d4992f415555220fe8
jay <dj.min43@gmail.com>
2021-11-01 07:19:08 +0900

    go: sort

--- go-lang-try/standard-library/main.go
+++ go-lang-try/standard-library/main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"sort"
 )
 
 func main() {
-	greeting := "hello there friends!"
+	// greeting := "hello there friends!"
 
 	// fmt.Println(strings.Contains(greeting, "hello"))
 	// fmt.Println(strings.ReplaceAll(greeting, "hello", "hi"))
-	fmt.Println(strings.ToUpper(greeting))
+	// fmt.Println(strings.ToUpper(greeting))
+	// fmt.Println(strings.Index(greeting, "ll"))
+	// fmt.Println(strings.Split(greeting, "l"))
 
+	ages := []int{45, 20, 35, 30, 75, 60, 50, 25}
+
+	sort.Ints(ages)
+	fmt.Println(ages)
 
 }
