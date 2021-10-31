2e386660dc4960efb93a83e6ca339b73390f6377
jay <dj.min43@gmail.com>
2021-11-01 08:28:44 +0900

    go: if else

--- go-lang-try/boolean/main.go
+++ go-lang-try/boolean/main.go
@@ -9,4 +9,12 @@ func main() {
 	fmt.Println(age >= 50)
 	fmt.Println(age == 45)
 	fmt.Println(age != 50)
+
+	if age < 30 {
+		fmt.Println("age is less than 30")
+	} else if age < 40 {
+		fmt.Println("age is less than 40")
+	} else {
+		fmt.Println("age is not less than 45")
+	}
 }
