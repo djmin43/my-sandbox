bca7b6676882a36490331ba3608a3bb05cd1939e
jay <dj.min43@gmail.com>
2021-11-02 08:33:25 +0900

    go: multiple return values

---
+++ go-lang-try/multiple-return-values/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func getInitials(n string) (string, string) {
+	s := strings.ToUpper(n)
+	names := strings.Split(s, " ")
+
+	var initials []string
+	for _, v := range names {
+		initials = append(initials, v[:1])
+	}
+
+	if len(initials) > 1 {
+		return initials[0], initials[1]
+	}
+
+	return initials[0], "_"
+
+}
+
+func main() {
+	fn, sn := getInitials("tifa lockhart")
+	fmt.Println(fn, sn)
+}
