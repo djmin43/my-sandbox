6f7086587510db293790aa19c50b6ed275e6cc3a
jay <dj.min43@gmail.com>
2021-11-03 08:08:04 +0900

    maps;

--- go-lang-try/maps/main.go
+++ go-lang-try/maps/main.go
@@ -12,7 +12,27 @@ func main() {
 		"toffee pudding": 3.55,
 	}
 
-	fmt.Prinln(menu)
+	fmt.Println(menu)
 	fmt.Println(menu["soup"])
 
+	// looping maps
+	for key, value := range menu {
+		fmt.Println(key, value)
+	}
+
+	// ints as key type
+	phoneobok := map[int]string{
+		12413824214: "James",
+		21234891234: "John",
+		31238449234: "Robert",
+		4123490123: "Michael",
+	}
+	fmt.Println(phoneobok)
+	fmt.Println(phoneobok[21234891234])
+
+	phoneobok[12345678901] = "Mary"
+	fmt.Println(phoneobok)
+
+	phoneobok[12345678901] = "yoshi"
+	fmt.Println(phoneobok)
 }
