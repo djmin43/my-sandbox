b0ef48bde186ea8245cdf8383b1f34a004020e94
jay <dj.min43@gmail.com>
2021-11-03 08:03:47 +0900

    map

--- go-lang-try/maps/main.go
+++ go-lang-try/maps/main.go
@@ -1,8 +1,18 @@
 package main
 
+import "fmt"
+
 
 func main() {
 
-	menu := map[string]float64
+	menu := map[string]float64{
+		"soup": 4.99,
+		"pie": 7.99,
+		"salad": 6.99,
+		"toffee pudding": 3.55,
+	}
+
+	fmt.Prinln(menu)
+	fmt.Println(menu["soup"])
 
 }
