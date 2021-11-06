1d1c95cddd24b178a9db8f5ced78acacd54ee021
jay <dj.min43@gmail.com>
2021-11-06 17:55:33 +0900

    go: lerarn more about pointer wrapper types

--- go-lang-try/pass-by-value/main.go
+++ go-lang-try/pass-by-value/main.go
@@ -4,15 +4,30 @@ import (
 	"fmt"
 )
 
-func updateName(x string) {
+func updateName(x string) string {
 	x = "wedge"
+	return x
+}
+
+func updateMenu(y map[string]float64) {
+	y["coffee"] = 2.99
 }
 
 func main() {
-	// group A types => strings, ints, bools, floats, structs
+	// non-pointer types => strings, ints, bools, floats, structs
 	name := "tifa"
 
-	updateName(name)
+	name = updateName(name)
 
 	fmt.Println(name)
+
+	// pointer wrapper types => slices, maps, functions
+	menu := map[string]float64{
+		"pie":				5.95,
+		"ice cream": 	3.99,
+	}
+
+	updateMenu(menu)
+	fmt.Println(menu)
+
 }
