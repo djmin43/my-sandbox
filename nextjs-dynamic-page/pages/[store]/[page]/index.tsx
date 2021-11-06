a76e89bb74ede155f76cb01ed34c0fc36373fb1c
jay <dj.min43@gmail.com>
2021-11-06 14:17:27 +0900

    add a folder for little go book

--- nextjs-dynamic-page/pages/[store]/[page]/index.tsx
+++ nextjs-dynamic-page/pages/[store]/[page]/index.tsx
@@ -1,9 +1,17 @@
 import React, { ReactNode } from 'react'
+import Head from 'next/head'
 
 const index = (): ReactNode => {
   return (
     <div>
+      <Head />
+      <h1>
+        this is header!
+      </h1>
       hello index
+      <div className="contianer">
+        hello my name is container
+      </div>
     </div>
   )
 }
