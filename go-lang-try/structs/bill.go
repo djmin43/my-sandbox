a4fe8459e0276b7e0aad732b247e245d08a84c24
jay <dj.min43@gmail.com>
2021-11-07 07:29:40 +0900

    go: new bill

--- go-lang-try/structs/bill.go
+++ go-lang-try/structs/bill.go
@@ -5,3 +5,14 @@ type bill struct {
 	items map[string]float64
 	tip float64
 }
+
+// make new bills
+func newBill(name string) bill {
+	b := bill{
+		name: name,
+		items: map[string]float64{},
+		tip: 0,
+	}
+
+	return b
+}
