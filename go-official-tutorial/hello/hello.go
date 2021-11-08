633cdf09ce786717fae5845265b7fe5d833127aa
jay <dj.min43@gmail.com>
2021-11-09 08:37:51 +0900

    func

--- go-official-tutorial/hello/hello.go
+++ go-official-tutorial/hello/hello.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("catcat")
+
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
