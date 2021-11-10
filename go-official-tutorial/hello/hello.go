c25530e2298b1872f7523a12abf02c8ee1f73645
jay <dj.min43@gmail.com>
2021-11-10 19:29:17 +0900

    go random hello

--- go-official-tutorial/hello/hello.go
+++ go-official-tutorial/hello/hello.go
@@ -12,7 +12,7 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("")
+	message, err := greetings.Hello("mindongjoon")
 
 	if err != nil {
 		log.Fatal(err)
