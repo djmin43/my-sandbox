d1bfb45b7430c8073a8b7cb8f7f7b136e9483263
jay <dj.min43@gmail.com>
2021-11-16 07:31:03 +0900

    import gin

--- go-restful/main.go
+++ go-restful/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 type album struct {
 	ID     string  `json:"id"`
@@ -13,6 +17,16 @@ var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func main() {
+	// initialize gin router with Default
+	router := gin.Default()
+	// GET function to handle path and function
+	router.GET("/albums", getAlbums)
+
+	// Run func to attach the router to http Server and start the server
+	router.Run("localhost:8080")
 }
 
 // getAlbums responds with the list of all albums as JSON.
