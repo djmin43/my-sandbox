a44639d4972fcaa50601783555f9919faa6c92a4
jay <dj.min43@gmail.com>
2021-11-16 09:06:47 +0900

    save

--- go-web-app/wiki.go
+++ go-web-app/wiki.go
@@ -1,1 +1,13 @@
 package main
+
+import "io/ioutil"
+
+type Page struct {
+	Title string
+	Body []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
