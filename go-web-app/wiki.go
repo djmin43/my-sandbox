352bc34d7e6f94dde4e34b7252b6ec0a08665bb6
jay <dj.min43@gmail.com>
2021-11-18 06:57:25 +0900

    created a file

--- go-web-app/wiki.go
+++ go-web-app/wiki.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"io/ioutil"
+	"fmt"
 	"os"
 )
 
@@ -12,11 +12,21 @@ type Page struct {
 
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
-	return ioutil.WriteFile(filename, p.Body, 0600)
+	return os.WriteFile(filename, p.Body, 0600)
 }
 
 func loadPage(title string) *Page {
 	filename := title + ".txt"
-	body, _ := os.ReadFile(filename)
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil
+	}
 	return &Page{Title: title, Body: body}
+}
+
+func main() {
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	p1.save()
+	p2 := loadPage("TestPage")
+	fmt.Println(string(p2.Body))
 }
