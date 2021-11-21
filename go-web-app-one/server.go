1d82649a6016f123b2927ba93c0c953451b7f4c4
jay <dj.min43@gmail.com>
2021-11-22 08:28:19 +0900

    hello handler

--- go-web-app-one/server.go
+++ go-web-app-one/server.go
@@ -16,3 +16,17 @@ func main() {
 		log.Fatal(err)
 	}
 }
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Hello!")
+}
