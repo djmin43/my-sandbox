7e3a0b77b644e5e7871aeaf9a1c188f928257d79
jay <dj.min43@gmail.com>
2021-11-22 08:19:41 +0900

    handle func

--- go-web-app-one/server.go
+++ go-web-app-one/server.go
@@ -7,6 +7,10 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprintf(w, "Hello!")
+	})
+
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
