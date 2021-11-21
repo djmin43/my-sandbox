bcfdffe1d5f4aec79440a64426fd1695f958ea46
jay <dj.min43@gmail.com>
2021-11-22 08:39:49 +0900

    add index.html

--- go-web-app-one/server.go
+++ go-web-app-one/server.go
@@ -6,17 +6,6 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprintf(w, "Hello!")
-	})
-
-	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
-}
-
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
 		http.Error(w, "404 not found.", http.StatusNotFound)
@@ -29,4 +18,13 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(w, "Hello!")
+}
+
+func main() {
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
