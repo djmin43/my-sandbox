f853deb44e57023d89623e2c5e04cfcff1d67555
jay <dj.min43@gmail.com>
2021-11-22 08:40:49 +0900

    add file server

--- go-web-app-one/server.go
+++ go-web-app-one/server.go
@@ -21,6 +21,8 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
 	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
