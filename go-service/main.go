cba2465b0c8cda2ea8822945eb9baa67f709b87c
jay <dj.min43@gmail.com>
2021-11-23 07:47:45 +0900

    module

--- go-service/main.go
+++ go-service/main.go
@@ -1,10 +1,28 @@
 package main
 
-import "github.com/kardianos/service"
+import (
+	"fmt"
+
+	"github.com/kardianos/service"
+)
 
 const serviceName = "Medium service"
 const serviceDescription = "Simple service, just for fun"
 
-func main() {
+type program struct{}
 
+func main() {
+	serviceConfig := &service.Config{
+		Name:						serviceName,
+		DisplayName: 		serviceName,
+		Description:		serviceDescription,
+	}
+	prg := &program{}
+	s, err := service.New(prog, serviceConfig)
+	if err != nil {
+		fmt.Println("Cannot create the service" + err.Error())
+	}
+	if err = s.Run(); err != nil {
+		fmt.Println("Cannot start the service: " + err.Error())
+	}
 }
