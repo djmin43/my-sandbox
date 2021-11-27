8ded37635a98274432ff8ca3616da7d6a392a090
jay <dj.min43@gmail.com>
2021-11-27 16:19:18 +0900

    part 3 done

--- go-service/homepage.go
+++ go-service/homepage.go
@@ -2,10 +2,28 @@ package main
 
 import (
 	"net/http"
+	"text/template"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
 )
 
+type HomePage struct {
+	Time string
+}
+
 func serveHomepage(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
-	
+	writingSync.Lock()
+	programIsRunning = true
+	writingSync.Unlock()
+
+	var homepage HomePage
+	homepage.Time = time.Now().String()
+
+	tmpl := template.Must(template.ParseFiles("html/homepage.html"))
+	_ = tmpl.Execute(writer, homepage)
+
+	writingSync.Lock()
+	programIsRunning = false
+	writingSync.Unlock()
 }
