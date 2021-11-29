381cb22e2d6cebfe7f686c16572e31c8b5362877
jay <dj.min43@gmail.com>
2021-11-30 08:14:23 +0900

    add button time to js

--- go-service/js/homepage.js
+++ go-service/js/homepage.js
@@ -10,3 +10,9 @@ time.addEventListener('time', (e) => {
     document.getElementById("actual-time").innerHTML = "Actual time using SSE: " + e.data;
 
 }, false);
+
+const buttonTime = document.getElementById("button-time")
+
+testButton.addEventListener("click", function () {
+    buttonTime.textContent = "Button clicked at: " + Date.now().toString()
+})
