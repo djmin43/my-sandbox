2fcf5a9fba9e07ed95f7a251268bead30c1393f8
jay <dj.min43@gmail.com>
2021-12-01 08:39:50 +0900

    debugging

--- go-service/js/homepage.js
+++ go-service/js/homepage.js
@@ -9,7 +9,6 @@ testButton.addEventListener("click", function () {
 })
 
 const time = new EventSource('/time');
-console.log(time);
 time.addEventListener('time', (e) => {
     document.getElementById("actual-time").innerHTML = "Actual time using SSE: " + e.data;
 }, false);
@@ -23,21 +22,21 @@ testButton.addEventListener("click", function () {
 askButton.addEventListener("click", function () {
     let data = {
         Name: name.value,
-        Time: new Data().toLocaleString("en-IE"),
-    }
+        Time: new Date().toLocaleString("en-IE"),
+    };
     fetch("/get_time", {
-        header: {
+        headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
         method: "POST",
-        body:JSON.stringify(data)
+        body: JSON.stringify(data)
     }).then((response) => {
         response.text().then(function (data) {
             let result = JSON.parse(data);
             console.log(result)
-        })
+        });
     }).catch((error) => {
         console.log(error)
-    })
+    });
 })
