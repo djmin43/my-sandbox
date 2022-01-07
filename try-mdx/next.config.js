e584cf6b719c07e3d8f87c44f7ce138d3efdeaff
jay <dj.min43@gmail.com>
2022-01-08 08:52:46 +0900

    feat: add mdx

--- try-mdx/next.config.js
+++ try-mdx/next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+})
+
 module.exports = {
   reactStrictMode: true,
+  pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
 }
+
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+})
--- try-mdx/pages/index.tsx
+++ try-mdx/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import LandingPage from ''
 
 const Home: NextPage = () => {
   return (
